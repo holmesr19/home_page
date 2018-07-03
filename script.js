6df4e3d2657916b78a6b06c276a4d611356202a1
holmesr19 <holmesr19@mail.wlu.edu>
2018-07-02 23:00:06 -0400

    added experimental "show more" button plus additional content

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMore() {
+    var x = document.getElementById("209_hidden");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
